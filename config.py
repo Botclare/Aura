4be8f082c49e2453285f42250269cfc06d58cd4b
Botclare <botclare@gmail.com>
2025-02-15 21:16:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1700))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6955568347))
+OWNER_ID = int(getenv("OWNER_ID", None))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,11 +39,11 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Sanatan_ke_hindu")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DIL_SE_SANATANI")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Teamfoxbots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Teamfoxbots")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", Flase))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
